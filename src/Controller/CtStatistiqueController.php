8dd55b8ac6bab36be2e685515e632560aba1b5a1
kaira <husaynmozart@gmail.com>
2020-09-23 14:04:32 +0200

    avancement avant pdf

--- src/Controller/CtStatistiqueController.php
+++ src/Controller/CtStatistiqueController.php
@@ -15,6 +15,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 class CtStatistiqueController extends AbstractController
 {
     /**
@@ -82,5 +85,37 @@ class CtStatistiqueController extends AbstractController
     public function recherche(): Response
     {
         return $this->render('ct_statistique/recherche.html.twig');
+    }
+
+    /**
+     * @Route("/ct/stat/pdf", name="ct_pdf_historique", methods={"GET", "POST"})
+     */
+    public function pdf(Request $request)
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('default/mypdf.html.twig', [
+            'title' => "Welcome to our PDF Test"
+        ]);
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => true
+        ]);
     }
 }
