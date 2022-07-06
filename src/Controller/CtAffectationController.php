5151340f56b724d65ca60b7d7b2c84d71c21d141
kaira <husaynmozart@gmail.com>
2022-07-06 09:33:28 +0300

    pagination de la liste

--- src/Controller/CtAffectationController.php
+++ src/Controller/CtAffectationController.php
@@ -10,21 +10,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Knp\Component\Pager\PaginatorInterface;
 
 class CtAffectationController extends AbstractController
 {
     /**
      * @Route("/ct/affectation/liste", name="ct_liste")
      */
-    public function liste(): Response
+    public function liste(Request $request, PaginatorInterface $paginator): Response
     {
-        $ctUsers = $this->getDoctrine()
-            ->getRepository(CtUser::class)
-            ->findAll();
+        $pagination = $paginator->paginate(
+            $ctUsers = $this->getDoctrine()->getRepository(CtUser::class)->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            20/*limit per page*/
+        );
+        //$ctUsers = $this->getDoctrine()->getRepository(CtUser::class)->findAll();
 
         return $this->render('ct_affectation/liste.html.twig', [
             'controller_name' => 'Ct Affectation liste',
-            'ct_users' => $ctUsers,
+            'ct_users' => $pagination,
         ]);
     }
 
