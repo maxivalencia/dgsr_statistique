28343ee17b3b7960b841f0ea9139a79fb9fcff16
kaira <husaynmozart@gmail.com>
2022-08-17 10:14:29 +0300

    avancement liste et affectation utilisateur

--- src/Form/CtAffectationType.php
+++ src/Form/CtAffectationType.php
@@ -12,9 +12,15 @@ class CtAffectationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            ->add('username', null, [
+                'label' => 'Nom utilisateur',
+                'disabled' => 'True',
+            ])
             //->add('usernameCanonical')
-            //->add('email')
+            ->add('email', null, [
+                'label' => 'mail',
+                'disabled' => 'True',
+            ])
             //->add('emailCanonical')
             //->add('enabled')
             //->add('salt')
@@ -23,7 +29,10 @@ class CtAffectationType extends AbstractType
             //->add('confirmationToken')
             //->add('passwordRequestedAt')
             //->add('roles')
-            //->add('usrName')
+            ->add('usrName', null, [
+                'label' => 'Nom et prénom',
+                'disabled' => 'True',
+            ])
             //->add('usrEmail')
             //->add('usrLocked')
             //->add('usrPassword')
@@ -37,6 +46,10 @@ class CtAffectationType extends AbstractType
             //->add('usrTelephone')
             //->add('usrIsConnected')
             //->add('usrPresence')
+            ->add('ctRole', null, [
+                'label' => 'Rôle',
+                'disabled' => 'True',
+            ])
             ->add('ctCentre', null, [
                 'attr' => [
                     'class' => 'js-example-basic-multiple',
